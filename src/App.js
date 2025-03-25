dbe0ae1a2d550300826155e4a99b5e4d5680638f
Chintha Venu Gopal <109978977+GopalChinta@users.noreply.github.com>
2025-03-25 07:46:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from react-router-dom;
 import Login from './src/Login';
 import Dashboard from './src/Dashboard';
 import MapView from './src/MapView';
